fac237ece638942f713a0ba5a94b7c2e19459de1
Tiago Mazzon <tiago.mazzon@gmail.com>
2023-03-17 12:45:14 -0300

    feat: inicio curso 3

---
+++ aluroni-router-curso3/src/pages/Inicio/index.tsx
@@ -0,0 +1,12 @@
+import cardapio from 'pages/Cardapio/Itens/itens.json';
+
+export default function Inicio() {
+  return (
+    <section>
+      <h3>Recomendações da Cozinha</h3>
+      <div>
+
+      </div>
+    </section>
+  );
+}
